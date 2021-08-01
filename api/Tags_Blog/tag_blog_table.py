829f299eac90b89b74151cd20f98f0b3c5184b5b
Daniel Lwetabe <dannylwe11@gmail.com>
2021-08-01 22:18:40 +0300

    minor formatting

--- api/Tags_Blog/tag_blog_table.py
+++ api/Tags_Blog/tag_blog_table.py
@@ -1,5 +1,6 @@
 from api import db
 
-tag_blog = db.Table('tag_blog', db.Column('tag_id', db.Interger, db.ForeignKey('tag.id'), primary_key=True),
-db.Column('blog_id', db.Integer, db.ForeignKey('blog.id'), primary_key=True)
+tag_blog = db.Table('tag_blog', 
+    db.Column('tag_id', db.Interger, db.ForeignKey('tag.id'), primary_key=True),
+    db.Column('blog_id', db.Integer, db.ForeignKey('blog.id'), primary_key=True)
 )
