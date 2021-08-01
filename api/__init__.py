1768eb06931502675ec37e3b20f876962d032ef4
Daniel Lwetabe <dannylwe11@gmail.com>
2021-08-01 22:17:32 +0300

    setup blog and tag models as well as many-to-many relationship between blog and tags

---
+++ api/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+db =  SQLAlchemy()
+
+def create_app():
+    """
+    Initialises the app and the database
+    """
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskdatabase.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    CORS(app)
+
+    db.init_app(app)
+    return app
